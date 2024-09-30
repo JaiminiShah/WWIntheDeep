6187c5d0dfe99bdd2e50dcb52ee14ad590c2e743
JaiminiShah <shahjaiminij@gmail.com>
2024-09-30 19:15:19 -0400

    jaiminishah

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WW_Auto2024.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.SECONDS;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="WW_Auto2024",group="WiredWoodmen")
+public class WW_Auto2024 extends LinearOpMode {
+    public enum START_POSITION {
+        BLUE_LEFT,
+        BLUE_RIGHT,
+        RED_LEFT,
+        RED_RIGHT
+    }
+
+    public static START_POSITION startPosition;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        selectStartingPosition();
+        telemetry.addData("Selected Starting Position", startPosition);
+        // Wait for the DS start button to be touched.
+
+        telemetry.addLine("The starting point of the robot is assumed to be on the starting tile, " +
+                "and along the edge of alliance chamber. ");
+
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        //Game Play Button  is pressed
+        if (opModeIsActive() && !isStopRequested()) {
+            //Build parking trajectory based on last detected target by vision
+            runAutonoumousMode();
+        }
+    }   // end runOpMode()
+    public void runAutonoumousMode() {
+        //Initialize Pose2d as desired
+        Pose2d initPose = new Pose2d(0, 0, 0); // Starting Pose
+        Pose2d moveBeyondTrussPose = new Pose2d(0, 0, 0);
+        Pose2d dropPurplePixelPose = new Pose2d(0, 0, 0);
+        Pose2d midwayPose1 = new Pose2d(0, 0, 0);
+        Pose2d midwayPose1a = new Pose2d(0, 0, 0);
+        Pose2d intakeStack = new Pose2d(0, 0, 0);
+        Pose2d midwayPose2 = new Pose2d(0, 0, 0);
+        Pose2d dropYellowPixelPose = new Pose2d(0, 0, 0);
+        Pose2d parkPose = new Pose2d(0, 0, 0);
+        double waitSecondsBeforeDrop = 0;
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
+
+        initPose = new Pose2d(0, 0, Math.toRadians(0)); //Starting pose
+        moveBeyondTrussPose = new Pose2d(15, 0, 0);
+    }
+
+        //Method to select starting position using X, Y, A, B buttons on gamepad
+    public void selectStartingPosition() {
+        telemetry.setAutoClear(true);
+        telemetry.clearAll();
+        //******select start pose*****
+        while(!isStopRequested()){
+
+            telemetry.addData("---------------------------------------","");
+            telemetry.addLine("This Auto program uses Open CV Vision Processor for Team Element detection");
+            telemetry.addData("Select Starting Position using XYAB on Logitech (or ▢ΔOX on Playstayion) on gamepad 1:","");
+            telemetry.addData("    Blue Left   ", "(X / ▢)");
+            telemetry.addData("    Blue Right ", "(Y / Δ)");
+            telemetry.addData("    Red Left    ", "(B / O)");
+            telemetry.addData("    Red Right  ", "(A / X)");
+            if(gamepad1.x){
+                startPosition = START_POSITION.BLUE_LEFT;
+                break;
+            }
+            if(gamepad1.y){
+                startPosition = START_POSITION.BLUE_RIGHT;
+                break;
+            }
+            if(gamepad1.b){
+                startPosition = START_POSITION.RED_LEFT;
+                break;
+            }
+            if(gamepad1.a){
+                startPosition = START_POSITION.RED_RIGHT;
+                break;
+            }
+            telemetry.update();
+        }
+        telemetry.clearAll();
+    }
+    //method to wait safely with stop button working if needed. Use this instead of sleep
+    public void safeWaitSeconds(double time) {
+        ElapsedTime timer = new ElapsedTime(SECONDS);
+        timer.reset();
+        while (!isStopRequested() && timer.time() < time) {
+        }
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WW_Teleop2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WW_Teleop2024.java
@@ -1,107 +1,175 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="WW_Teleop2024", group="GreenwoodFTC") //declares the name of the class and the
+
+@TeleOp(name="WW_Teleop2024", group="WiredWoodmen")  //declares the name of the class and the
+// group it is in.
+
+
 
 public class WW_Teleop2024 extends OpMode{
-    double
-            DRIVE_TRAIN_SPEED = 0.7,
-            controllerDeadZone = 0.3;
+
+
+    double  //declares all double variables and their values
+            speedVariable = .8;
+
+    //float[] hsvValuesRight = new float[3];
+    //float[] hsvValuesLeft = new float[3];
+
+
+
+
+    /*
+     * Code will run ONCE when the driver hits INIT
+     * INIT means initialize
+     */
     DcMotorEx
-            frontLeftDrive = null,
-            frontRightDrive = null,
-            rearLeftDrive = null,
-            rearRightDrive = null;
+            rearLeftDrive= null,
+            rearRightDrive = null,
+            frontLeftDrive= null,
+            frontRightDrive = null;
+    // pixelArm = null;
+
     HardwareMap hwMap =  null;
     ElapsedTime runTime = new ElapsedTime();
 
 
     @Override
-    public void init() {
-        //Configuring the RobotController with the same device name and Mapping device to HardwareMap
+    public void init() { //initialization class to be used at start of tele-op
+
+        // these are our motors and what they are called
         rearLeftDrive=hardwareMap.get(DcMotorEx.class, "rear_left_drive");
 
         rearRightDrive = hardwareMap.get(DcMotorEx.class, "rear_right_drive");
         frontLeftDrive = hardwareMap.get(DcMotorEx.class, "front_left_drive");
         frontRightDrive = hardwareMap.get(DcMotorEx.class, "front_right_drive");
-        // Direction
-        frontLeftDrive.setDirection(DcMotorEx.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotorEx.Direction.REVERSE);
-        rearLeftDrive.setDirection(DcMotorEx.Direction.FORWARD);
-        rearRightDrive.setDirection(DcMotorEx.Direction.FORWARD);
-        //Resetting the Encoder
-        frontLeftDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        frontRightDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rearLeftDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rearRightDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        //Setting motors to run without encoders
-        frontLeftDrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        frontRightDrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        rearLeftDrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        rearRightDrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        // Setting power to zero
+
+//Direction?
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rearRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        // pixelArm.setDirection(DcMotor.Direction.REVERSE);
+
         frontLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
         rearLeftDrive.setPower(0);
         rearRightDrive.setPower(0);
 
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        //Setting motors to run without encoders
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        //this will send a telemetry message to signify robot waiting;
+        telemetry.addLine("AUTOBOTS ROLL OUT");
+        telemetry.update();
+        runTime.reset();
 
     }
 
+    /*
+     * Code will run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     *this code will run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code will run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
     @Override
     public void loop() {
+
+        //==========================================================
+        //                        GamePad One
+        //==========================================================
+
+        //Controls Drive Train
 
         float FLspeed = -gamepad1.left_stick_y + gamepad1.left_stick_x;
         float BLspeed = -gamepad1.left_stick_y - gamepad1.left_stick_x;
         float FRspeed = -gamepad1.right_stick_y - gamepad1.right_stick_x;
         float BRspeed = -gamepad1.right_stick_y + gamepad1.right_stick_x;
 
+        rearLeftDrive.setPower(Range.clip((-BLspeed*speedVariable),-1,1));
+        rearRightDrive.setPower(Range.clip((BRspeed*speedVariable),-1,1));
+        frontLeftDrive.setPower(Range.clip((FLspeed*speedVariable),-1,1));
+        frontRightDrive.setPower(Range.clip((-FRspeed*speedVariable),-1,1));
 
-        telemetry.addData("Value of Left_stick Y", gamepad1.left_stick_y);
-        telemetry.addData("Value of Left Stick X", gamepad1.left_stick_x);
-        telemetry.addData("Value of Right Stick Y", gamepad1.right_stick_y);
-        telemetry.addData("Value of Right Stick X", gamepad1.right_stick_x);
-        telemetry.addData("FLspeed", FLspeed);
-        telemetry.addData("BLspeed", BLspeed);
-        telemetry.addData("FRspeed", FRspeed);
-        telemetry.addData("BRspeed", BRspeed);
-        telemetry.update();
 
-        if (false) {
-            rearLeftDrive.setPower(BLspeed * DRIVE_TRAIN_SPEED);
-            rearRightDrive.setPower(BRspeed * DRIVE_TRAIN_SPEED);
-            frontLeftDrive.setPower(FLspeed * DRIVE_TRAIN_SPEED);
-            frontRightDrive.setPower(FRspeed * DRIVE_TRAIN_SPEED);
-        } else {
-            boolean left_dead_y = Math.abs(gamepad1.left_stick_y) < controllerDeadZone;
-            boolean left_dead_x = Math.abs(gamepad1.left_stick_x) < controllerDeadZone;
-            boolean right_dead_x = Math.abs(gamepad1.right_stick_x) < controllerDeadZone;
-            boolean right_dead_y = Math.abs(gamepad1.right_stick_y) < controllerDeadZone;
-            if (left_dead_x && left_dead_y && right_dead_x && right_dead_y) {
-                frontLeftDrive.setPower(0);
-                frontRightDrive.setPower(0);
-                rearLeftDrive.setPower(0);
-                rearRightDrive.setPower(0);
-            } else {
-                rearLeftDrive.setPower(BLspeed * DRIVE_TRAIN_SPEED);
-                rearRightDrive.setPower(BRspeed * DRIVE_TRAIN_SPEED);
-                frontLeftDrive.setPower(FLspeed * DRIVE_TRAIN_SPEED);
-                frontRightDrive.setPower(FRspeed * DRIVE_TRAIN_SPEED);
-            }
-        }
+        //DriveTrain Speed Controls
+        if (gamepad1.dpad_left) speedVariable -= 0.05;
+        if (gamepad1.dpad_right) speedVariable += 0.05;
+        speedVariable= Range.clip(speedVariable,0,1);
+
+        //==========================================================
+        //                        GamePad Two
+        //==========================================================
+
+        //D-Pad stuff. none right noe
+
+
+        //==========================================================//
+        //                        Telemetry                           //
+        //==========================================================/
+       telemetrymotorprint();
 
     }
+    public void telemetrymotorprint(){
+        telemetry.clear();
+        telemetry.addData("Drive Train Speed: " , speedVariable);
+        telemetry.addData("BRMotor2", "Position : %2d, Power : %.2f", rearRightDrive.getCurrentPosition(), rearRightDrive.getPower());
+        telemetry.addData("FRMotor2", "Position : %2d, Power : %.2f", frontRightDrive.getCurrentPosition(), frontRightDrive.getPower());
+
+        telemetry.addData("FLMotor2", "Position : %2d, Power : %.2f", frontLeftDrive.getCurrentPosition(), frontLeftDrive.getPower());
+        telemetry.addData("BLMotor2", "Position : %2d, Power : %.2f", rearLeftDrive.getCurrentPosition(), rearLeftDrive.getPower());
+        telemetry.addLine("left joystick | ")
+                .addData("x", gamepad1.left_stick_x)
+                .addData("y", gamepad1.left_stick_y);
+        telemetry.addLine("right joystick | ")
+                .addData("x", gamepad1.right_stick_x)
+                .addData("y", gamepad1.right_stick_y);
+
+        // this will send a telemetry message to signify robot waiting
+        telemetry.addLine("I 'm Ready");
+        telemetry.update();
+
+    }
+
+
+    /*
+     * Code will run ONCE after the driver hits STOP
+     */
     @Override
     public void stop(){
+        // Sets all motors to zero power except Arms to keep pos
         frontLeftDrive.setPower(0);
         rearLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
         rearRightDrive.setPower(0);
-
+        // pixelArm.setTargetPosition(pixelArm.getCurrentPosition());
     }
 }
